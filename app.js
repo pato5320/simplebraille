b9e49724f9ace425449c40e67c72cea3762f7f57
laureano <laureano5320@gmail.com>
2023-06-29 15:48:31 -0300

    working

--- app.js
+++ app.js
@@ -31,21 +31,86 @@ const letters = {
     "nothing": [0,0,0,0,0,0]
 }
 
-let index = Math.floor(Math.random() * 26)
-console.log('asd')
+let index;
+let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+let model;
+let user;
 
-const dots = document.querySelectorAll('.dot')
+console.log('letter ' + alphabet[index])
+localStorage.setItem('score', '0')
 
-for(elem in dots){
-    dot = dots[elem]
-    dot.addEventListener('click', function(e){
-        if (this.classList.contains('dot')){
-            this.classList.remove('dot')
-            this.classList.add('notdot')
+let modelIndex = 0
 
-        }else{
-            this.classList.remove('notdot')
-            this.classList.add('dot')
+resetNumber()
+setModel()
+
+
+const dotsUser = document.querySelectorAll('.pieceUser div div')
+
+for(elem in dotsUser){
+    dot = dotsUser[elem]
+    if(dot instanceof HTMLDivElement){
+        
+        dot.addEventListener('click', function(e){
+            if (this.classList.contains('dot')){
+                user[this.id] = 0
+                this.classList.remove('dot')
+                this.classList.add('notdot')
+
+            }else{
+                user[this.id] = 1
+                
+
+                this.classList.remove('notdot')
+                this.classList.add('dot')
+            }
+            
+        })
+    }
+}
+
+let check  = document.getElementById('check')
+
+check.addEventListener('click', function(e){
+    if(model.length === user.length && 
+    model.every(function(value, index) { return value === user[index]}) ){
+        score = Number(localStorage.getItem('score'))
+        score++;
+        localStorage.setItem('score', score)
+        score = localStorage.getItem('score')
+        scoreString = document.getElementById('scoreString')
+        scoreString.innerHTML = 'score: ' + score
+        resetNumber()
+        setModel()
+    }
+})
+
+function setModel(){
+    const dotsModel = document.querySelectorAll('.pieceModel div div')
+
+    for(elem in dotsModel){
+        dot = dotsModel[elem]
+    
+        if(dot instanceof HTMLDivElement){
+            
+            if(model[modelIndex] == 0){
+                dot.classList.remove('dot')
+                dot.classList.add('notdot')
+            }else{
+                dot.classList.remove('notdot')
+                dot.classList.add('dot')
+            }
+    
+            modelIndex++;
+            
+            
         }
-    })
+    }
+}
+
+function resetNumber(){
+    index = Math.floor(Math.random() * 26)
+    model = letters[alphabet[index]]
+    user = letters['nothing']
+    modelIndex = 0
 }
